090a0146c80dc426137d06413ee4d6b4cbf5e813
295290968@qq.com <295290968@qq.com>
2020-05-09 17:35:44 +0800

    预付款流程修改

--- src/com/weavernorth/saiwen/myWeb/WebUtil.java
+++ src/com/weavernorth/saiwen/myWeb/WebUtil.java
@@ -5,6 +5,15 @@ import weaver.conn.RecordSet;
 public class WebUtil {
 
     /**
+     * 创建供应商银行账号
+     */
+    public static String createSupplierBankAccountFromXML(String myXml, String orgId) throws Exception {
+        String[] userInfo = getUserInfo();
+        SevenU9WeaverLocator locator = new SevenU9WeaverLocator();
+        SevenU9WeaverSoap_PortType soap = locator.getSevenU9WeaverSoap();
+        return soap.createSupplierBankAccountFromXML(myXml, userInfo[0], userInfo[1], orgId);
+    }
+    /**
      * 付款单创建
      */
     public static String createPayBillFromXML(String myXml, String orgId) throws Exception {
